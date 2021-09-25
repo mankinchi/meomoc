64a540acecbc9c6104dbec4a360997dbd0dde947
Tri Nguyen <mankinchi.1995@gmail.com>
2021-09-25 22:33:28 +1000

    add new route

---
+++ pages/api/fb-verify.ts
@@ -0,0 +1,21 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(
+	req: NextApiRequest,
+	res: NextApiResponse,
+) {
+	const query = req.query;
+	const mode = query['hub.mode'];
+	const challenge = query['hub.challenge'];
+	const token = query['hub.verify_token'];
+
+	console.log(mode, challenge, token);
+
+	if (mode === 'subscribe' && token === process.env.FB_VERIFY_TOKEN) {
+		return res.status(200).send(challenge);
+	}
+
+	res.json({
+		error: 'wrong token'
+	});
+}
