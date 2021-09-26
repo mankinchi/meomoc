1e08adf8180c2d3a0e690562d0fc951ca4694700
Tri Nguyen <mankinchi.1995@gmail.com>
2021-09-27 00:31:14 +1000

    add prisma and try to add to db

--- pages/api/integration/fb.ts
+++ pages/api/integration/fb.ts
@@ -1,7 +1,32 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import util from 'util';
+import { PrismaClient } from '@prisma/client';
 
-export default function handler(
+interface FacebookWebhookAPI {
+	object: string,
+	entry: [
+		{
+			id: string,
+			time: number,
+			changes: [
+				{
+					value: {
+						link: string,
+						message: string,
+						post_id: string,
+						created_time: number,
+						item: string,
+						photo_id: string,
+						published: number,
+						verb: string,
+					},
+					field: string,
+				}
+			]
+		}
+	]
+}
+
+export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse,
 ) {
@@ -10,16 +35,46 @@ export default function handler(
 	const challenge = query['hub.challenge'];
 	const token = query['hub.verify_token'];
 
-	if (req.method === 'POST') {
-		console.log(util.inspect(req.body, false, null, true));
-		return res.status(200).end();
-	}
-
+	// To verify subscription
 	if (mode === 'subscribe' && token === process.env.FB_VERIFY_TOKEN) {
 		return res.status(200).send(challenge);
 	}
 
-	return res.json({
-		error: 'wrong token',
-	});
+	// To handle webhook
+
+	if (req.method === 'POST') {
+		const {
+			entry: [
+				{
+					changes: [
+						{
+							value: {
+								post_id,
+								created_time,
+							},
+						},
+					],
+				},
+			],
+		} = req.body as FacebookWebhookAPI;
+
+		const prisma = new PrismaClient();
+		try {
+			await prisma.$connect();
+			await prisma.post.create({
+				data: {
+					postId: post_id,
+					dateTime: new Date(created_time),
+				},
+			});
+		} catch (e) {
+			console.error(e);
+		} finally {
+			await prisma.$disconnect();
+		}
+
+		return res.status(200).end();
+	}
+
+	return res.status(500);
 }
