885a7365e7381086aee428afead85103f31fc912
Tri Nguyen <mankinchi.1995@gmail.com>
2021-09-29 18:41:35 +1000

    remove unused files

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css';
-import type { AppProps } from 'next/app';
-
-function MyApp({ Component, pageProps }: AppProps) {
-	// eslint-disable-next-line
-	return <Component {...pageProps} />;
-}
-export default MyApp;
