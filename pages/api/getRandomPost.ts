0c0f7ce079fd504d8b87210dc6f340bf519a06e7
Tri Nguyen <mankinchi.1995@gmail.com>
2021-09-29 18:42:13 +1000

    clean up api using utils

--- pages/api/getRandomPost.ts
+++ pages/api/getRandomPost.ts
@@ -1,76 +1,21 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { requestFBGraphAPI } from '../../utils/integration/fb';
+import { getPostDetailFromFB } from '../../utils/integration/fb';
 import { prisma } from '../../utils/db';
-
-interface FacebookImageDetails {
-	height: number;
-	src: string;
-	width: number;
-}
-
-enum FacebookPostType {
-	PHOTO = 'photo',
-	ALBUM = 'album',
-}
-
-interface FacebookAPIGetPostDetails {
-	attachments: {
-		data: [
-			{
-				media: {
-					image: FacebookImageDetails;
-				},
-				subattachments?: {
-					data: [
-						{
-							media: {
-								image: FacebookImageDetails;
-							},
-							type: FacebookPostType;
-						},
-					]
-				},
-				description?:string;
-				title?: string;
-				type: FacebookPostType;
-				url: string;
-			},
-		]
-	}
-}
-
-export interface GetRandomPostResponse {
-	link: string;
-	caption: string;
-	images: string[];
-}
+import { Post } from '../../types/api/post';
 
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse<GetRandomPostResponse>,
+	res: NextApiResponse<Post>,
 ) {
 	try {
 		await prisma.$connect();
 
 		const randomPost = await prisma.post.findFirst();
 		if (randomPost) {
-			const { postId } = randomPost;
+			const { id, postId } = randomPost;
+			const fbPost = await getPostDetailFromFB(id, postId);
 
-			const {
-				attachments: {
-					data: [data],
-				},
-			} = await requestFBGraphAPI<FacebookAPIGetPostDetails>(`/${postId}?fields=attachments`);
-
-			const post: GetRandomPostResponse = {
-				caption: data.title || data.description || '',
-				link: data.url,
-				images: data.type === FacebookPostType.PHOTO
-					? [data.media.image.src]
-					: data.subattachments!.data.map((subattachment) => subattachment.media.image.src),
-			};
-
-			res.json(post);
+			res.json(fbPost);
 		}
 	} catch (e) {
 		console.error(e);
