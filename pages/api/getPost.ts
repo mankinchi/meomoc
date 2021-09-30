8be8baf050da3f333019559b18c0453c20414cdd
Tri Nguyen <mankinchi.1995@gmail.com>
2021-09-30 12:20:13 +1000

    retrieve more data from getPost api

--- pages/api/getPost.ts
+++ pages/api/getPost.ts
@@ -3,23 +3,74 @@ import { getPostDetailFromFB } from '../../utils/integration/fb';
 import { prisma } from '../../utils/db';
 import { Post } from '../../types/api/post';
 
+export interface GetPostAPI {
+	post?: Post;
+	previousPostId?: string;
+	nextPostId?: string;
+}
+
 export default async function handler(
 	req: NextApiRequest,
-	res: NextApiResponse<Post>,
+	res: NextApiResponse<GetPostAPI>,
 ) {
 	try {
 		await prisma.$connect();
 
+		const postId = req.query.postId as string;
+
 		const dbPost = await prisma.post.findFirst({
 			where: {
-				id: req.query.postId as string,
+				id: postId,
 			},
 		});
-		if (dbPost) {
-			const fbPost = await getPostDetailFromFB(dbPost.id, dbPost.postId);
 
-			res.json(fbPost);
+		let previousPostId: string | undefined;
+		let nextPostId: string | undefined;
+
+		if (dbPost) {
+			const getPreviousPostId = prisma.post.findFirst({
+				where: {
+					dateTime: {
+						lt: dbPost.dateTime,
+					},
+				},
+				select: {
+					id: true,
+				},
+				orderBy: {
+					dateTime: 'desc',
+				},
+			});
+
+			const getNextPostId = prisma.post.findFirst({
+				where: {
+					dateTime: {
+						gt: dbPost.dateTime,
+					},
+				},
+				select: {
+					id: true,
+				},
+				orderBy: {
+					dateTime: 'asc',
+				},
+			});
+
+			const [previousPost, nextPost] = await Promise.all([
+				getPreviousPostId,
+				getNextPostId,
+			]);
+
+			if (previousPost) previousPostId = previousPost.id;
+			if (nextPost) nextPostId = nextPost.id;
 		}
+
+		const fbPost = dbPost ? await getPostDetailFromFB(dbPost.id, dbPost.postId) : undefined;
+		res.json({
+			post: fbPost,
+			previousPostId,
+			nextPostId,
+		});
 	} catch (e) {
 		console.error(e);
 	} finally {
